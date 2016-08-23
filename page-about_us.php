c0d1b09b818581dad620de64aa93152a5cdeebec
Tim Normington <tim.j.normington@gmail.com>
2016-08-23 09:19:34 -0700

    Template edits
    1. Added about us page template
    2. Modified front page template to add social links

---
+++ page-about_us.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: Two Column (even)
+*/
+?>
+
+<?php get_header(); ?>
+
+	<div id="content">
+
+		<div id="inner-content" class="row">
+
+		    <main id="main-left" class="llarge-6 small-12 columns">
+					<div class="panel">
+						<h1 class="block-header"><?php the_title(); ?></h1>
+						<h2> HELLOW WORLD</h2>
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<?php get_template_part( 'parts/loop', 'left-column' ); ?>
+
+				<?php endwhile; endif; ?>
+				<?php wp_reset_query(); ?>
+				
+
+			</div>
+
+			</main> <!-- end #main -->
+
+			<main id="main-right" class="large-6 small-12 columns">
+				<?php //var_dump(get_field('right_column_content')); ?>
+				<?php if('' !== get_field('right_column_content')): ?>
+					<div class="panel">
+						<?php get_template_part( 'parts/loop', 'right-column' ); ?>
+					</div>
+				<?php endif; ?>
+
+			</main>
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
